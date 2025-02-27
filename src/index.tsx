6d27e539410f2ba287e5bfa3fcadae961a6d0d28
Ferry Clarence Tan <tanferryclarence@gmail.com>
2025-02-27 08:55:43 +0800

    Modify index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,16 @@
-// Insert your base code here
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router'
+
+import App from './App'
+
+document.body.innerHTML = '<div id="root"></div>'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
